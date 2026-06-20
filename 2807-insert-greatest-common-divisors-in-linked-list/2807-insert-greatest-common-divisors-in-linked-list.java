ea7f548ed33b897c9941dd155b18a5b90a38161c
AMOGHAVARSHA HEGDE <amoghavarshahegde@gmail.com>
2026-06-20 22:29:00 +0530

    Time: 1 ms (100%), Space: 47.3 MB (28.31%) - LeetHub

---
+++ 2807-insert-greatest-common-divisors-in-linked-list/2807-insert-greatest-common-divisors-in-linked-list.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+      public int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+    public ListNode insertGreatestCommonDivisors(ListNode head) {
+        ListNode temp = head;
+
+        while (temp.next != null) {
+            int g = gcd(temp.val, temp.next.val);
+
+            ListNode node = new ListNode(g);
+            node.next = temp.next;
+            temp.next = node;
+
+            temp = node.next;
+        }
+
+        return head;
+    }
+}
