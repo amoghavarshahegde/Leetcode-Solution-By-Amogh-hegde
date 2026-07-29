cd8c57ce9dc6892d2626367c24607566d87c6078
AMOGHAVARSHA HEGDE <amoghavarshahegde@gmail.com>
2026-07-29 11:10:17 +0530

    Time: 2 ms (94.8%), Space: 85.3 MB (91.91%) - LeetHub

--- 2874-maximum-value-of-an-ordered-triplet-ii/2874-maximum-value-of-an-ordered-triplet-ii.java
+++ 2874-maximum-value-of-an-ordered-triplet-ii/2874-maximum-value-of-an-ordered-triplet-ii.java
@@ -7,7 +7,7 @@ class Solution {
         rightMax[n - 1] = nums[n - 1];
 
         for (int i = n - 2; i >= 0; i--) {
-            rightMax[i] = Math.max(rightMax[i], rightMax[i + 1]);
+         
             rightMax[i] = Math.max(nums[i], rightMax[i + 1]);
         }
 
