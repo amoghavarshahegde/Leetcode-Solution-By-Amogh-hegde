5d5b9a2ca2545dfaf2448174ae5964abaabea2fe
AMOGHAVARSHA HEGDE <amoghavarshahegde@gmail.com>
2026-08-15 11:48:41 +0530

    Time: 1 ms (97.59%), Space: 47.6 MB (22.62%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int total=0;
+        for(int num:nums){
+            total+=num;
+        }
+        int left_s=0;
+        for(int i=0;i<nums.length;i++){
+           int right_s=total-left_s-nums[i];
+
+
+            if(left_s==right_s){
+                return i;
+            }
+            left_s+=nums[i];
+        }
+        return -1;
+        
+    }
+}
