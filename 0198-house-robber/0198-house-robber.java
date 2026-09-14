7f48098d5db97ae39675b938925d77c9fc3e4524
AMOGHAVARSHA HEGDE <amoghavarshahegde@gmail.com>
2026-09-14 19:47:31 +0530

    Time: 0 ms (100%), Space: 42.9 MB (37.64%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int rob(int[] nums) {
+
+        int[] dp = new int[nums.length];
+
+        Arrays.fill(dp, -1);
+
+        return solve(nums, 0, dp);
+    }
+
+    public int solve(int[] nums, int i, int[] dp) {
+
+        if (i >= nums.length) {
+            return 0;
+        }
+
+        if (dp[i] != -1) {
+            return dp[i];
+        }
+
+        int take = nums[i] + solve(nums, i + 2, dp);
+
+        int skip = solve(nums, i + 1, dp);
+
+        dp[i] = Math.max(take, skip);
+
+        return dp[i];
+    }
+}
