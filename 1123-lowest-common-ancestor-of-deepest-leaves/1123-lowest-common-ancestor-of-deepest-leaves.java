30fc9876b90d534f88a785f30f7acf543761343b
AMOGHAVARSHA HEGDE <amoghavarshahegde@gmail.com>
2026-09-14 19:07:42 +0530

    Time: 0 ms (100%), Space: 46.4 MB (46.65%) - LeetHub

---
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.java
@@ -0,0 +1,36 @@
+class Solution {
+
+    class Pair {
+        TreeNode node;
+        int depth;
+
+        Pair(TreeNode node, int depth) {
+            this.node = node;
+            this.depth = depth;
+        }
+    }
+
+    public TreeNode lcaDeepestLeaves(TreeNode root) {
+        return dfs(root).node;
+    }
+
+    private Pair dfs(TreeNode root) {
+
+        if (root == null) {
+            return new Pair(null, 0);
+        }
+
+        Pair left = dfs(root.left);
+        Pair right = dfs(root.right);
+
+        if (left.depth == right.depth) {
+            return new Pair(root, right.depth + 1);
+        }
+
+        if (left.depth > right.depth) {
+            return new Pair(left.node, left.depth + 1);
+        }
+
+        return new Pair(right.node, right.depth + 1);
+    }
+}
